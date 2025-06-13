5327b836734ec66e54fd0cb08e4d1cdb3831184f
LuisPorto <55835054+luis52627@users.noreply.github.com>
2025-06-13 14:29:44 +0100

    Update kodi_monitor.py
    Compatível com Python 3.
    Uso de super() para inicialização correta do xbmc.Monitor
    Compatibilidade com bytes e str para data (como exigido em Python 3)
    log(...) com nível de aviso apropriado
    Comentários antigos e referências obsoletas removidos.

--- resources/lib/kodi_monitor.py
+++ resources/lib/kodi_monitor.py
@@ -1,34 +1,35 @@
 #!/usr/bin/python
+
 import xbmc
 import time
+import json
 from resources.lib.helper import *
 
 class KodiMonitor(xbmc.Monitor):
 
     def __init__(self, **kwargs):
-        xbmc.Monitor.__init__(self)
+        super(KodiMonitor, self).__init__()
         self.win = kwargs.get('win')
 
     def onDatabaseUpdated(self, database):
         pass
 
     def onNotification(self, sender, method, data):
-        #log('SchaumermalHelper_Monitor: sender %s - method: %s  - data: %s' % (sender, method, data))
         try:
             mediatype = ''
-            data = json.loads(data.decode('utf-8'))
-            if data and isinstance(data, dict):
-                if data.get('item'):
+            if isinstance(data, bytes):  # garantir compatibilidade
+                data = data.decode('utf-8')
+            data = json.loads(data)
+            if isinstance(data, dict):
+                if 'item' in data:
                     mediatype = data['item'].get('type', '')
-                elif data.get('type'):
-                    mediatype = data['type']
-            if method == 'Player.OnStop':
-                if mediatype == 'episode':
-                    self.refresh_widget('nextepisodes')
+                elif 'type' in data:
+                    mediatype = data.get('type', '')
+            if method == 'Player.OnStop' and mediatype == 'episode':
+                self.refresh_widget('nextepisodes')
         except Exception as ex:
-            log('Exception in SchaumermalHelper_Monitor: %s' % ex)
-
+            log(f'Exception in KodiMonitor: {ex}', xbmc.LOGWARNING)
 
     def refresh_widget(self, widget):
-        prop = 'widgetreload-' + widget
+        prop = f'widgetreload-{widget}'
         self.win.setProperty(prop, time.strftime("%Y%m%d%H%M%S", time.gmtime()))
