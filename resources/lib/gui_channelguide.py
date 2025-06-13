358512ebb03e9f74a47c57043465b98b45b958a2
LuisPorto <55835054+luis52627@users.noreply.github.com>
2025-06-13 14:01:03 +0100

    Update gui_channelguide.py
    Compatível com Python 3.
    PVR InfoLabels Missing:
    ListItem.ChannelLogo
    ListItem.NextGenre(separator)
    Pvr.NowRecordingChannelIcon
    Pvr.NextRecordingChannelIcon
    Pvr.EPGEventIcon

--- resources/lib/gui_channelguide.py
+++ resources/lib/gui_channelguide.py
@@ -1,16 +1,17 @@
-#!/usr/bin/python
-# coding: utf-8
-import xbmc, xbmcgui, xbmcvfs
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import xbmc
+import xbmcgui
+import xbmcvfs
+import xbmcaddon
 from resources.lib.helper import *
-#######################################################################################
 
-ADDON               = xbmcaddon.Addon()
+ADDON = xbmcaddon.Addon()
 
-#######################################################################################
+class Gui_ChannelGuide(xbmcgui.WindowXMLDialog):
 
-class Gui_ChannelGuide( xbmcgui.WindowXMLDialog ):
-    
-    def __init__( self, *args, **kwargs ):
+    def __init__(self, *args, **kwargs):
         self.channelgroups = None
         self.detail_active = False
         self.channels_loaded = self.loadChannels()
@@ -18,42 +19,36 @@ class Gui_ChannelGuide( xbmcgui.WindowXMLDialog ):
     def loadChannels(self):
         if not self.loadChannelGroups():
             return False
-
         for index, group in enumerate(self.channelgroups):
-            id = group['channelgroupid']
-            query = json_call('PVR.GetChannels',
-                    properties = channeldetail_properties, 
-                    params={ 'channelgroupid': id } )
             try:
+                query = json_call('PVR.GetChannels',
+                                  properties=channeldetail_properties,
+                                  params={'channelgroupid': group['channelgroupid']})
                 self.channelgroups[index]['channels'] = query['result']['channels']
                 self.channelgroups[index]['channellistitems'] = None
-            except Exception:
-                log('error loading channels', WARNING)
+            except Exception as e:
+                log(f'error loading channels: {e}', xbmc.LOGWARNING)
                 return False
         return True
 
     def loadChannelGroups(self):
-        query = json_call('PVR.GetChannelGroups',
-                    params={'channeltype': 'tv'} )
         try:
+            query = json_call('PVR.GetChannelGroups', params={'channeltype': 'tv'})
             self.channelgroups = query['result']['channelgroups']
         except Exception:
             return False
-        log("loaded groups: %s" % self.channelgroups, DEBUG)
-        hide_all_channels = xbmc.getCondVisibility('Skin.HasSetting(hide_all_channels)')
-        if hide_all_channels:
-            allchannels = -1
+        log(f"loaded groups: {self.channelgroups}", xbmc.LOGDEBUG)
+
+        if xbmc.getCondVisibility('Skin.HasSetting(hide_all_channels)'):
             str_allchannels = xbmc.getLocalizedString(19287)
-            for index, group in enumerate(self.channelgroups):
-                if group['label'] == str_allchannels:
-                    allchannels = index
-                    break
-            if allchannels > -1:
-                del self.channelgroups[allchannels]
-        log("groups after hide_all_channels: %s" % self.channelgroups, DEBUG)
+            self.channelgroups = [
+                group for group in self.channelgroups
+                if group['label'] != str_allchannels
+            ]
+        log(f"groups after hide_all_channels: {self.channelgroups}", xbmc.LOGDEBUG)
         return True
 
-    def onInit( self ):
+    def onInit(self):
         self.hor_layout = xbmc.getCondVisibility('Skin.HasSetting(use_channelgroups_fullwidth)')
         if not self.channels_loaded:
             return
@@ -71,7 +66,7 @@ class Gui_ChannelGuide( xbmcgui.WindowXMLDialog ):
         xbmc.executebuiltin('ClearProperty(loadingchannels,10608)')
 
     def onClick(self, control_id):
-        if not control_id == 13:
+        if control_id != 13:
             return
         group_index = self.list_channelgroups.getSelectedPosition()
         channel_index = self.list_channels.getSelectedPosition()
@@ -83,16 +78,16 @@ class Gui_ChannelGuide( xbmcgui.WindowXMLDialog ):
         self.switchChannel(channel_uid)
 
     def onAction(self, action):
-        if action.getId() == 92:
-            self._close()
-        elif action.getId() == 1:
-            self.keyLeft()
-        elif action.getId() == 2:
-            self.keyRight()
-        elif action.getId() == 3:
-            self.keyUp()
-        elif action.getId() == 4:
-            self.keyDown()
+        actions = {
+            92: self._close,
+            1: self.keyLeft,
+            2: self.keyRight,
+            3: self.keyUp,
+            4: self.keyDown
+        }
+        func = actions.get(action.getId())
+        if func:
+            func()
 
     def _close(self):
         self.clearProperty('showdetail')
@@ -101,19 +96,19 @@ class Gui_ChannelGuide( xbmcgui.WindowXMLDialog ):
 
     def keyLeft(self):
         focus = self.getFocusId()
-        if focus == 13 and not self.detail_active:
-            self.setFocusId(12)
-        elif focus == 13 and self.detail_active:
-            self.clearProperty('showdetail')
-            self.detail_active = False
+        if focus == 13:
+            if self.detail_active:
+                self.clearProperty('showdetail')
+                self.detail_active = False
+            else:
+                self.setFocusId(12)
         elif focus == 12:
             self._close()
 
     def keyRight(self):
-        focus = self.getFocusId()
-        if focus == 12:
+        if self.getFocusId() == 12:
             self.setFocusId(13)
-        elif focus == 13:
+        elif self.getFocusId() == 13:
             self.setProperty('showdetail', 'true')
             self.detail_active = True
 
@@ -125,7 +120,7 @@ class Gui_ChannelGuide( xbmcgui.WindowXMLDialog ):
             self.updateChannels()
         elif focus == 13:
             self.channel_index = self.list_channels.getSelectedPosition()
-            if self.channel_index == len(self.channelgroups[self.group_index]['channels'])-1 and self.jump_to_next_group:
+            if self.channel_index == len(self.channelgroups[self.group_index]['channels']) - 1 and self.jump_to_next_group:
                 self.groupUp()
 
     def keyDown(self):
@@ -140,17 +135,13 @@ class Gui_ChannelGuide( xbmcgui.WindowXMLDialog ):
                 self.groupDown()
 
     def groupUp(self):
-        self.group_index -= 1
-        if self.group_index == -1:
-            self.group_index = len(self.channelgroups) - 1
+        self.group_index = (self.group_index - 1) % len(self.channelgroups)
         self.list_channelgroups.selectItem(self.group_index)
-        self.channel_index = len(self.channelgroups[self.group_index]['channels'])-1
+        self.channel_index = len(self.channelgroups[self.group_index]['channels']) - 1
         self.updateChannels()
 
     def groupDown(self):
-        self.group_index += 1
-        if self.group_index == len(self.channelgroups):
-            self.group_index = 0
+        self.group_index = (self.group_index + 1) % len(self.channelgroups)
         self.list_channelgroups.selectItem(self.group_index)
         self.channel_index = 0
         self.updateChannels()
@@ -177,15 +168,15 @@ class Gui_ChannelGuide( xbmcgui.WindowXMLDialog ):
             self.list_channelgroups.addItem(listitem)
 
     def positionChannellist(self):
-        if self.hor_layout: return
-        x = 100
-        y = 0
+        if self.hor_layout:
+            return
+        x, y = 100, 0
         height = 1080
         max_items = 11
         num_channels = len(self.channelgroups[self.group_index]['channels'])
         if num_channels < max_items:
             height = num_channels * 100
-            y = int((1080 - height)/2)
+            y = int((1080 - height) / 2)
         self.list_channels.setHeight(height)
         self.list_channels.setPosition(x, y)
 
@@ -194,70 +185,76 @@ class Gui_ChannelGuide( xbmcgui.WindowXMLDialog ):
         utc_offset = getUtcOffset()
         for channel in self.channelgroups[self.group_index]['channels']:
             try:
+                now = channel['broadcastnow']
+                next_ = channel['broadcastnext']
                 listitem = xbmcgui.ListItem(channel['label'])
-                listitem.setArt({ 'icon': channel['icon'] })
+                listitem.setArt({'icon': channel['icon']})
                 listitem.setProperty('channelnumber', str(channel['channelnumber']))
-                listitem.setProperty('isrecording', str(channel['broadcastnow']['hastimer']))
-                listitem.setProperty('progress', str(int(channel['broadcastnow']['progresspercentage'])))
-                listitem.setProperty('now_title', channel['broadcastnow']['title'])
-                listitem.setProperty('now_episodename', channel['broadcastnow']['episodename'])
-                listitem.setProperty('now_episodenum', str(channel['broadcastnow']['episodenum']))
-                listitem.setProperty('now_year', str(channel['broadcastnow']['year']))
-                listitem.setProperty('now_director', channel['broadcastnow']['director'])
-                listitem.setProperty('now_genre', ', '.join(channel['broadcastnow']['genre']))
-                listitem.setProperty('now_cast', channel['broadcastnow']['cast'])
-                listitem.setProperty('now_plot', channel['broadcastnow']['plot'])
-                starttime = getTimeFromString(channel['broadcastnow']['starttime'], '%Y-%m-%d %H:%M:%S', utc_offset)
-                endtime = getTimeFromString(channel['broadcastnow']['endtime'], '%Y-%m-%d %H:%M:%S', utc_offset)
-                listitem.setProperty('now_starttime', starttime.strftime('%H:%M'))
-                listitem.setProperty('now_endtime', endtime.strftime('%H:%M'))
-                listitem.setProperty('now_runtime', str(channel['broadcastnow']['runtime']))
-                listitem.setProperty('next_title', channel['broadcastnext']['title'])
-                listitem.setProperty('next_episodename', channel['broadcastnext']['episodename'])
-                listitem.setProperty('next_episodenum', str(channel['broadcastnext']['episodenum']))
-                listitem.setProperty('next_year', str(channel['broadcastnext']['year']))
-                listitem.setProperty('next_director', channel['broadcastnext']['director'])
-                listitem.setProperty('next_genre', ', '.join(channel['broadcastnext']['genre']))
-                listitem.setProperty('next_cast', channel['broadcastnext']['cast'])
-                listitem.setProperty('next_plot', channel['broadcastnext']['plot'])
-                starttime_next = getTimeFromString(channel['broadcastnext']['starttime'], '%Y-%m-%d %H:%M:%S', utc_offset)
-                endtime_next = getTimeFromString(channel['broadcastnext']['endtime'], '%Y-%m-%d %H:%M:%S', utc_offset)
-                listitem.setProperty('next_starttime', starttime_next.strftime('%H:%M'))
-                listitem.setProperty('next_endtime', endtime_next.strftime('%H:%M'))
-                listitem.setProperty('next_runtime', str(channel['broadcastnext']['runtime']))
+                listitem.setProperty('isrecording', str(now.get('hastimer', False)))
+                listitem.setProperty('progress', str(int(now.get('progresspercentage', 0))))
+                listitem.setProperty('now_title', now.get('title', ''))
+                listitem.setProperty('now_episodename', now.get('episodename', ''))
+                listitem.setProperty('now_episodenum', str(now.get('episodenum', '')))
+                listitem.setProperty('now_year', str(now.get('year', '')))
+                listitem.setProperty('now_director', now.get('director', ''))
+                listitem.setProperty('now_genre', ', '.join(now.get('genre', [])))
+                listitem.setProperty('now_cast', now.get('cast', ''))
+                listitem.setProperty('now_plot', now.get('plot', ''))
+                st = getTimeFromString(now['starttime'], '%Y-%m-%d %H:%M:%S', utc_offset)
+                et = getTimeFromString(now['endtime'], '%Y-%m-%d %H:%M:%S', utc_offset)
+                listitem.setProperty('now_starttime', st.strftime('%H:%M') if st else '')
+                listitem.setProperty('now_endtime', et.strftime('%H:%M') if et else '')
+                listitem.setProperty('now_runtime', str(now.get('runtime', '')))
+
+                listitem.setProperty('next_title', next_.get('title', ''))
+                listitem.setProperty('next_episodename', next_.get('episodename', ''))
+                listitem.setProperty('next_episodenum', str(next_.get('episodenum', '')))
+                listitem.setProperty('next_year', str(next_.get('year', '')))
+                listitem.setProperty('next_director', next_.get('director', ''))
+                listitem.setProperty('next_genre', ', '.join(next_.get('genre', [])))
+                listitem.setProperty('next_cast', next_.get('cast', ''))
+                listitem.setProperty('next_plot', next_.get('plot', ''))
+                stn = getTimeFromString(next_['starttime'], '%Y-%m-%d %H:%M:%S', utc_offset)
+                etn = getTimeFromString(next_['endtime'], '%Y-%m-%d %H:%M:%S', utc_offset)
+                listitem.setProperty('next_starttime', stn.strftime('%H:%M') if stn else '')
+                listitem.setProperty('next_endtime', etn.strftime('%H:%M') if etn else '')
+                listitem.setProperty('next_runtime', str(next_.get('runtime', '')))
+
                 if channel['channelnumber'] == self.active_channel_number:
                     listitem.select(True)
                 self.channelgroups[self.group_index]['channellistitems'].append(listitem)
-            except Exception:
-                log('no epg for channel: %s' % Exception, WARNING )
+            except Exception as e:
+                log(f'no epg for channel: {e}', xbmc.LOGWARNING)
 
     def getActiveChannelNumber(self):
-        channel_num = xbmc.getInfoLabel('VideoPlayer.ChannelNumberLabel')
-        return int(channel_num)
+        try:
+            channel_num = xbmc.getInfoLabel('VideoPlayer.ChannelNumberLabel')
+            return int(channel_num)
+        except (ValueError, TypeError):
+            return -1
 
     def getActiveChannelIndex(self):
         for index, group in enumerate(self.channelgroups):
             for index_channel, channel in enumerate(group['channels']):
                 if channel['channelnumber'] == self.active_channel_number:
                     return (index, index_channel)
-        return (-1,-1)
+        return (-1, -1)
 
     def switchChannel(self, channel_uid):
-        all_channels_loc = xbmc.getLocalizedString(19287).encode('utf-8')
+        all_channels_loc = xbmc.getLocalizedString(19287)
         pvr_backend = self.pvrBackendAddonId()
-        if not pvr_backend: return
-        pvr_url = 'pvr://channels/tv/' + all_channels_loc + '/' + pvr_backend + '_' + str(channel_uid) + '.pvr'
-        action = 'PlayMedia(' + pvr_url + ')'
-        xbmc.executebuiltin(action)
+        if not pvr_backend:
+            return
+        pvr_url = f'pvr://channels/tv/{all_channels_loc}/{pvr_backend}_{channel_uid}.pvr'
+        xbmc.executebuiltin(f'PlayMedia({pvr_url})')
 
     def pvrBackendAddonId(self):
-        query_addons = json_call('Addons.GetAddons', params={ 'type': 'xbmc.pvrclient' }, properties=['enabled'])
         try:
+            query_addons = json_call('Addons.GetAddons', params={'type': 'xbmc.pvrclient'}, properties=['enabled'])
             addons = query_addons['result']['addons']
             for addon in addons:
-                if addon['enabled']:
-                    return addon['addonid'].encode('utf-8')
-            return None
-        except Exception:
-            log('error querying pvr addon: %s' % Exception, WARNING )
-            return None
+                if addon.get('enabled'):
+                    return addon['addonid']
+        except Exception as e:
+            log(f'error querying pvr addon: {e}', xbmc.LOGWARNING)
+        return None
